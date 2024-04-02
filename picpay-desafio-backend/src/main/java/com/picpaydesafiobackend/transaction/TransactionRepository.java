0aad1a57453f9d3b699d09562808f54fd57fc767
Carlos Henrique <ichfs9108@gmail.com>
2024-04-02 01:40:11 -0300

    feat: Criando camada repository

---
+++ picpay-desafio-backend/src/main/java/com/picpaydesafiobackend/transaction/TransactionRepository.java
@@ -0,0 +1,6 @@
+package com.picpaydesafiobackend.transaction;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface TransactionRepository extends ListCrudRepository<Transaction, Long> {
+}
